d36fdd3b486c83ffafaaf0952ced8c781a1aff2c
StellaFoxxie <stellastuffs@gmail.com>
2022-05-25 15:22:23 -0700

    worked a bunch on the audio functionallity
    i did this high so i hope it makes any sense whatsover. im prolly going to have to rewrite it anyway, with blackjack, and hookers

--- guestbook.js
+++ guestbook.js
@@ -1,10 +1,10 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 
 const GIST_ID="e97fd77282bb1ac24a1e5fa58bdc828f"
-const GH_TOKEN="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"
+const GH_TOKEN="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"
 
 const octokit = new Octokit({
-  auth: GH_TOKEN
+  //auth: GH_TOKEN
 })
 
 let book = await octokit.request('GET /gists/{gist_id}', {
@@ -16,3 +16,5 @@ console.log(book.data.files["Guestbook.json"].content);
 
 console.log("testing am i working?");
 console.log(octokit);
+
+//prolly going to scrap this, its a huge security issue
