42a00f9b7873722dfa81b0b1015949c2b9c01e27
StellaFoxxie <stellastuffs@gmail.com>
2022-05-26 00:14:55 -0700

    did lots of work on audio
    i got a system in place where i can specify audio tracks and have them automatically made into a playlist
    i also perfected the pannel changes including history support and url paramaters. EVERYTHING IS AWESOME

---
+++ index.js
@@ -0,0 +1,118 @@
+
+/////////////classes & large functions////////////{
+class MusicPlayer { //main music player for the header
+    constructor(callback) {
+        this.player = document.getElementById("header-player"); //////grabbing all the things it needs
+        this.playButton = document.getElementById("header-playPauseButton");
+        this.time = document.getElementById("header-time");
+        this.seeker = document.getElementById("header-seeker");
+        this.title = document.getElementById("header-title-bar");
+        
+        this.playlist = [];
+        let temp = Object.keys(musicData["songs"]); //creates a temporary array of strings based on the names of the music
+        for(var i = 0; i < temp.length; i+=1) {
+            this.playlist.push(musicData["songs"][temp[i]])
+        }
+        this.nowPlaying = 0;
+    }
+    load(songId) {
+        this.player.setAttribute("src",this.playlist[songId].src)
+        this.title.innerHTML = this.playlist[songId].name;
+        this.nowPlaying = songId;
+    }
+    
+    play() {
+        this.player.play();
+        this.playButton.value = "⏸"; //sets the button to the pause icon
+    }
+    pause() {
+        this.player.pause();
+        this.playButton.value = "▶️"; // sets the button to the play icon
+    }
+    playPause() {
+        if (this.player.paused) { //toggles the paused state
+            this.play();
+        } else {
+            this.pause();
+        }
+    }
+    update() {
+        ///generates the text saying how much time is remaining
+        this.time.innerHTML = 
+            (Math.floor(this.player.currentTime/60)).toString() + ":" + (Math.floor(this.player.currentTime%60)).toString().padStart(2,0) + " / " +
+            (Math.floor(this.player.duration/60)).toString() + ":" + (Math.floor(this.player.duration%60)).toString().padStart(2,0);
+        this.seeker.value = this.player.currentTime; ///sets the seeker slider to the right position
+        
+        if (this.player.ended) {
+            if (this.playlist[this.nowPlaying+1] != undefined) {
+                this.load(this.nowPlaying + 1);
+                this.play();
+            } else {
+                this.pause();
+            }
+        }
+    }
+    seek() {
+        if (!this.seeker.initialized) { //"initializes" the seeker
+            this.seeker.max = this.player.duration;
+            this.seeker.initialized = true;
+        }
+        this.player.currentTime = this.seeker.value; //actually sets the player position when you seek
+    }
+}
+
+function changePage(Page, noPush, noRefresh) {
+    tabs = document.getElementsByClassName("tabs");
+    
+    for(var i = 0; i < tabs.length; i+=1) {
+        if (tabs[i].getAttribute("class") == "tabs active") {
+            tabs[i].setAttribute("class","tabs");
+        }
+    }
+    document.getElementById(Page).setAttribute("class","tabs active");
+    
+    if (!noPush) {
+        history.pushState(Page, '', "?" + Page);
+    }
+    
+    if (!noRefresh) {
+        document.getElementsByTagName("iframe")[0].contentDocument.location.replace(Page + ".html");
+    }
+    
+    if (!noRefresh && noPush) {
+        history.replaceState(Page, '', "?" + Page);
+    }
+    //console.log(state);
+    page = Page;
+}
+//////////end of classes & large functions////////}
+preload(setup); ////pulls the script up by its bootstraps
+
+
+var page = "home";
+let musicData, gameData, musicPlayer;
+
+
+async function preload(callback) {
+    musicData = await fetch("assets/music/music.json").then(response => {return response.json();});
+    //gameData = await fetch("assets/p5games/games.json").then(response => {return response.json();});
+    musicPlayer = new MusicPlayer();
+    if (callback != undefined) callback();
+}
+
+
+function setup() {
+    musicPlayer.load(0);
+    setInterval(tick, 1000/20);
+    if (window.location.search != "") changePage(window.location.search.slice(1));
+}
+
+function tick() {
+    musicPlayer.update(); //runs the update function 20 times a second
+}
+
+addEventListener('popstate', event => {
+    if (history.state != null && history.state != page) changePage(history.state,true,true);
+    if (history.state == null) changePage("home",true);
+});
+
